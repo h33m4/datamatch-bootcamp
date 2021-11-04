4ad1e4fed9306300f045445d4ae59a99dc6be2bb
Adam Mohamed <adamshokrymohamed@gmail.com>
2021-11-04 12:21:33 -0400

    Completed CardViewer functionality, but bugs present

--- src/App.js
+++ src/App.js
@@ -10,12 +10,10 @@ class App extends React.Component {
         {front: 'front1', back: 'back1'},
         {front: 'front2', back: 'back2'},
       ],
-
-      currentCard:[],
+      currentIndex: 0,
+      currentFace: "front",
     };
   }
-
-
 
   addCard = (card) => {
     const cards = this.state.cards.slice().concat(card);
@@ -28,9 +26,49 @@ class App extends React.Component {
     this.setState({cards});
   }
 
+  getCurrentCard = () => {
+    if (this.state.currentFace === "front") {
+      return this.state.cards[this.state.currentIndex].front;
+    }
+    else if (this.state.currentFace === "back") {
+      return this.state.cards[this.state.currentIndex].back;
+    }
+  }
+
+  nextCard = () => {
+    if (this.state.currentIndex < this.state.cards.length - 1) {
+      this.setState({currentIndex: this.state.currentIndex + 1});
+      this.setState({currentFace: "front"});
+    }
+  }
+
+  prevCard = () => {
+    if (this.state.currentIndex > 0) {
+      this.setState({currentIndex: this.state.currentIndex - 1});
+      this.setState({currentFace: "front"});
+    }
+  }
+
+  switchFace = () => {
+    let newFace = this.state.currentFace === "front" ? "back" : "front";
+    this.setState({currentFace: newFace});
+  }
+  
   render() {
     return (
-    <><CardEditor addCard={this.addCard} deleteCard={this.deleteCard} cards={this.state.cards} /><CardViewer nextCard={this.nextCard}/></>
+    <>
+    <CardEditor 
+    addCard={this.addCard} 
+    deleteCard={this.deleteCard} 
+    cards={this.state.cards} />
+    <CardViewer 
+    nextCard={this.nextCard} 
+    prevCard={this.prevCard} 
+    getCurrentCard={this.getCurrentCard}
+    switchFace={this.switchFace}
+    currentIndex={this.state.currentIndex}
+    numCards={this.state.cards.length}/>
+    </>
     );
   }
 }
--- src/CardViewer.js
+++ src/CardViewer.js
@@ -3,18 +3,29 @@ import './CardViewer.css';
 
 class CardViewer extends React.Component {
     
+    getCurrentCard = () => this.props.getCurrentCard();
+
+    switchFace = () => this.props.switchFace();
+
+    prevCard = () => this.props.prevCard();
+    nextCard = () => this.props.nextCard();
 
     render() {
+        const currentCardFace = this.getCurrentCard();
+
         return (
             <div>
                 <h2>Card Viewer</h2>
-                <table class="flashcard">
-                    <td>
-                    Hello
-                    </td>
+                <table onClick={this.switchFace} className="flashcard">
+                    <tbody>
+                    <tr>
+                        <td>{currentCardFace}</td>
+                    </tr>
+                    </tbody>
                 </table>
-                <button>Previous Card</button>
-                <button>Next Card</button>
+                <button onClick={this.prevCard}>Previous Card</button>
+                <button onClick={this.nextCard}>Next Card</button>
+                <p>Card {this.props.currentIndex + 1}/{this.props.numCards}</p>
             </div>
 
         );
